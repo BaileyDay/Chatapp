f436c843a9c697130eed5a6e09bbb853328db9bb
Bailey Day <bailey.j.day@gmail.com>
2020-06-17 15:18:01 -0400

    Added auth route

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,3 +2,5 @@ const express = require("express");
 const router = express.Router();
 
 const authController = require("../controllers/auth");
+
+router.post("/register", authController.postSignup);
