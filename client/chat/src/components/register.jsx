17172dba78f810b959e17dbebea3d4e68c617abe
Bailey Day <bailey.j.day@gmail.com>
2020-06-17 17:21:39 -0400

    database recieving and sending information:

--- client/chat/src/components/register.jsx
+++ client/chat/src/components/register.jsx
@@ -2,43 +2,63 @@ import React from "react";
 import { Component } from "react";
 import "../css/main.css";
 import axios from "axios";
+import { Redirect, Link } from "react-router-dom";
 
 class Register extends Component {
   constructor(props) {
     super(props);
-    this.state = { apiResponse: "" };
+    this.state = {
+      apiResponse: "",
+      username: "",
+      password: "",
+      redirect: false,
+    };
+    this.createUser = this.createUser.bind(this);
   }
 
-  createUser() {
+  createUser(e) {
     axios
       .post("http://localhost:3002/register", {
-        firstName: "Fred",
-        lastName: "Flintstone",
+        username: this.state.username,
+        password: this.state.password,
       })
-      .then(function (response) {
-        console.log(response);
-      })
+      .then(function () {})
       .catch(function (error) {
         console.log(error);
       });
   }
 
   render() {
+    if (this.state.redirect === true) {
+      return <Redirect to="/login" />;
+    }
     return (
       <div className="container">
         <h1 className="loginLogo register">Register</h1>
         <form>
           <div>
-            <input type="text" placeholder="Enter Username" />
+            <input
+              type="text"
+              placeholder="Enter Username"
+              onChange={(event) =>
+                this.setState({ username: event.target.value })
+              }
+            />
           </div>
           <div>
-            <input type="text" placeholder="Enter Password" />
+            <input
+              type="text"
+              placeholder="Enter Password"
+              onChange={(event) =>
+                this.setState({ password: event.target.value })
+              }
+            />
           </div>
           <div>
             <input type="text" placeholder="Confirm Password" />
           </div>
           <button className="loginButton" onClick={this.createUser}>
-            Login
+            Register
           </button>
         </form>
       </div>
