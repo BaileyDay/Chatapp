c9b9f8aee30922c302c26058e6e9696c0fa5d95e
Bailey Day <bailey.j.day@gmail.com>
2020-06-18 00:37:24 -0400

    Got redirect working for registration

--- client/chat/src/components/register.jsx
+++ client/chat/src/components/register.jsx
@@ -1,68 +1,52 @@
-import React from "react";
-import { Component } from "react";
+import React, { useState } from "react";
 import "../css/main.css";
 import axios from "axios";
-import { Redirect, Link } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 
-class Register extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      apiResponse: "",
-      username: "",
-      password: "",
-      redirect: false,
-    };
-    this.createUser = this.createUser.bind(this);
-  }
+function Register() {
+  const [username, setUsername] = useState(0);
+  const [password, setPassword] = useState(0);
+  let history = useHistory();
 
-  createUser(e) {
+  function createUser(e) {
+    e.preventDefault();
     axios
       .post("http://localhost:3002/register", {
-        username: this.state.username,
-        password: this.state.password,
+        username: username,
+        password: password,
       })
-      .then(function () {})
       .catch(function (error) {
         console.log(error);
       });
+    history.push("/login");
   }
 
-  render() {
-    if (this.state.redirect === true) {
-      return <Redirect to="/login" />;
-    }
-    return (
-      <div className="container">
-        <h1 className="loginLogo register">Register</h1>
-        <form>
-          <div>
-            <input
-              type="text"
-              placeholder="Enter Username"
-              onChange={(event) =>
-                this.setState({ username: event.target.value })
-              }
-            />
-          </div>
-          <div>
-            <input
-              type="text"
-              placeholder="Enter Password"
-              onChange={(event) =>
-                this.setState({ password: event.target.value })
-              }
-            />
-          </div>
-          <div>
-            <input type="text" placeholder="Confirm Password" />
-          </div>
-          <button className="loginButton" onClick={this.createUser}>
-            Register
-          </button>
-        </form>
-      </div>
-    );
-  }
+  return (
+    <div className="container">
+      <h1 className="loginLogo register">Register</h1>
+      <form>
+        <div>
+          <input
+            type="text"
+            placeholder="Enter Username"
+            onChange={(event) => setUsername(event.target.value)}
+          />
+        </div>
+        <div>
+          <input
+            type="text"
+            placeholder="Enter Password"
+            onChange={(event) => setPassword(event.target.value)}
+          />
+        </div>
+        <div>
+          <input type="text" placeholder="Confirm Password" />
+        </div>
+        <button className="loginButton" onClick={createUser}>
+          Register
+        </button>
+      </form>
+    </div>
+  );
 }
 export default Register;
