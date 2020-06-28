7e7ae08b8bd00f25f38fb7f5899c88691666de20
Bailey Day <bailey.j.day@gmail.com>
2020-06-28 16:25:37 -0400

    Fixed pathing

--- app.js
+++ app.js
@@ -17,7 +17,11 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "build")));
+
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
 
 app.use(cors());
 app.use(authRoutes);
